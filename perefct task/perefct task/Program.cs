e33e20b864832b221518b2958d81ae5d6b330766
MohammedZaki011 <121100938+MohammedZaki011@users.noreply.github.com>
2022-12-20 14:56:47 +0200

    Add files via upload

---
+++ perefct task/perefct task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perefct_task
+{
+    internal class Program
+    {
+        static bool Isperefct(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0) sum = sum + i;
+            }
+            if (sum == number) return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls Enter the frist number: ");
+            int x = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("pls Enter the second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+
+            for (int i = x; i <= y; i++)
+            {
+                if (Isperefct(i)) Console.Write("{0},", i);
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
